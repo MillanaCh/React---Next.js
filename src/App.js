93d483d75cf1f8dd9cbf748d6edbb21d82c57206
millana_chotubaeva <chotubaevam@gmail.com>
2023-03-03 20:02:02 -0800

    update the windows for open and close logic

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import Backdrop from "./component/Backdrop";
-import Modal from "./component/Modal";
 import Todo from "./component/Todo";
 
 function App() {
   return (
     <div>
-       <h1>My todos</h1>
-      <Todo text="Learn React"/>
-      <Todo text="Learn Angular"/>
-      <Todo text="Learn Vue"/>
-      <Modal/>
-      <Backdrop/>
+      <h1>My todos</h1>
+      <Todo text="Learn React" />
+      <Todo text="Learn Angular" />
+      <Todo text="Learn Vue" />
     </div>
   );
 }
--- src/component/Todo.js
+++ src/component/Todo.js
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import Modal from "./Modal";
-
+import Backdrop from "./Backdrop";
 function Todo(props) {
-    
-    function deleteHandler(){
-        
-    }
+  const [modalIsOpen, setModelIsOpen] = useState(false);
+  function deleteHandler() {
+    setModelIsOpen(true);
+  }
+  function closeModalHandler(){
+    setModelIsOpen(false)
+  }
   return (
     <div className="card">
-      <h2>{props.text }</h2>
+      <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn" onClick={deleteHandler}>Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
+      {modalIsOpen ? <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler}/> : null}
+      {modalIsOpen && <Backdrop onCancel={closeModalHandler}/>}
     </div>
   );
 }
